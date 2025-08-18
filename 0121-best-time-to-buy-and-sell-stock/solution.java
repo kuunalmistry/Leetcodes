b3540b924e5df6f3be0b9bcc176402d1e6a72145
Kuunal Mistry <kuunalmistry@gmail.com>
2025-08-18 13:20:38 +0000

    [LeetCode Sync] Runtime - 2 ms (41.97%), Memory - 62.1 MB (99.93%)

---
+++ 0121-best-time-to-buy-and-sell-stock/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            // keep track of the lowest price so far
+            if (price < minPrice) {
+                minPrice = price;
+            } 
+            // calculate profit if sold today
+            else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
+            }
+        }
+
+        return maxProfit;
+        
+    }
+}
