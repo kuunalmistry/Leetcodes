773c8902c8156a15bf7cb655a80eaae4c539b586
Kuunal Mistry <kuunalmistry@gmail.com>
2025-11-04 12:36:02 +0000

    [LeetCode Sync] Runtime - 5 ms (82.96%), Memory - 77.5 MB (29.65%)

---
+++ 0011-container-with-most-water/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxArea(int[] height) {
+
+        int start = 0;
+        int end = height.length - 1;
+        int maxArea = 0;
+
+        while(start < end) {
+            maxArea = Math.max(maxArea,(end - start)*Math.min(height[start], height[end]));
+            
+            if(height[start] < height[end]) {
+                start++;
+            }
+            else {
+                end--;
+            }
+        }
+        return maxArea;
+    }
+
+    
+    
+}
+        
